771392c81f68248d6f0c476ef6249db96bc7f1e2
Sergey Veselov <veselovsa@list.ru>
2025-01-28 17:11:54 +0900

    fix:"fix footer"

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,14 +1,19 @@
 import styles from "./Footer.module.css";
+import { FooterProps } from "./Footer.props";
 
-export const Footer = () => {
+export const Footer = ({}: FooterProps) => {
   return (
     <footer className={styles.footer}>
       <div className={styles.data}>
         OwlTop &copy; 2020 - {new Date().getFullYear()} Все права защищены
       </div>
       <div className={styles.containerText}>
-        <div className={styles.text}>Пользовательское соглашение</div>
-        <div className={styles.text}>Политика конфиденциальности</div>
+        <a className={styles.text} href="#" target="_blank">
+          Пользовательское соглашение
+        </a>
+        <a className={styles.text} href="#" target="_blank">
+          Политика конфиденциальности
+        </a>
       </div>
     </footer>
   );
