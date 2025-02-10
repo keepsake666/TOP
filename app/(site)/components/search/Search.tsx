c110d31a0034ad814971d63373d73391d1dda749
Sergey Veselov <veselovsa@list.ru>
2025-02-10 11:09:03 +0900

    feat : "add component search"

---
+++ app/(site)/components/search/Search.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { KeyboardEvent } from "react";
+import { Button, Input } from "@/components";
+import styles from "./Search.module.css";
+import { SearchProps } from "./Search.props";
+import cn from "classnames";
+import { useState } from "react";
+import GlassIcon from "./glass.svg";
+import { useRouter } from "next/navigation";
+
+export const Search = ({ className, ...props }: SearchProps) => {
+  const [search, setSearch] = useState<string>("");
+  const route = useRouter();
+
+  const goToSearch = () => {
+    route.push(`/search/${search}`);
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      goToSearch();
+    }
+  };
+
+  return (
+    <form className={cn(styles.search, className)} {...props} role="search">
+      <Input
+        className={styles.input}
+        placeholder="Поиск..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        onKeyDown={handleKeyDown}
+      ></Input>
+      <Button
+        appearence="primary"
+        className={styles.button}
+        onClick={goToSearch}
+      >
+        <GlassIcon />
+      </Button>
+    </form>
+  );
+};
