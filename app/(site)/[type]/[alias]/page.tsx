cf9d120e2fa604127edf650848731e109aa50643
Sergey Veselov <veselovsa@list.ru>
2025-02-27 11:44:23 +0900

    fix: 'css product and static pages'

--- app/(site)/[type]/[alias]/page.tsx
+++ app/(site)/[type]/[alias]/page.tsx
@@ -1,6 +1,7 @@
 import { getPage } from "@/api/page";
 import { notFound } from "next/navigation";
 import TopPage from "../../components/top-page/TopPage";
+import { getMenu } from "@/api/menu";
 
 type Params = Promise<{ alias: string; type: string }>;
 
@@ -8,8 +9,15 @@ export async function generateMetadata({ params }: { params: Params }) {
   const { alias } = await params;
   const page = await getPage(alias);
   return {
-    title: page?.title,
+    title: page?.title || "Товары",
   };
+}
+
+export async function generateStaticParams() {
+  const menu = await getMenu(0);
+  return menu.flatMap((item) =>
+    item.pages.map((page) => ({ alias: page.alias, type: page.category }))
+  );
 }
 
 export default async function PageProducts({ params }: { params: Params }) {
